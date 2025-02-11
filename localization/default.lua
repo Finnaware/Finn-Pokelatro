711a6ad1848b2a8dc7ed987c7b612fe74e3bec8d
Finnaware <78937350+Finnaware@users.noreply.github.com>
2025-02-11 19:26:39 -0300

    Localization and more cards
    Added Localization
    
    Added Alolan Muk as Smeared Joker, Magikarp as Splash, Mega Rayquaza and Deoxys (Normal) as Satellite, AZ Floette as Vagabond
    
    Updated Acrobat Sprite

---
+++ localization/default.lua
@@ -0,0 +1,456 @@
+return {
+    descriptions={
+        Joker={
+            j_8_ball={
+                name="8 Ball",
+            },
+            j_abstract={
+                name="Ghastly",
+            },
+            j_acrobat={
+                name="Acrobats",
+            },
+            j_ancient={
+                name="Relicanth",
+            },
+            j_arrowhead={
+                name="Arrowhead",
+            },
+            j_astronomer={
+                name="Astronomer",
+            },
+            j_banner={
+                name="Chimecho",
+            },
+            j_baron={
+                name="Slowking",
+            },
+            j_baseball={
+                name="Lucario's Card",
+            },
+            j_blackboard={
+                name="Mimikyu Board",
+            },
+            j_bloodstone={
+                name="Bloodstone",
+            },
+            j_blue_joker={
+                name="Alolan Sandslash",
+            },
+            j_blueprint={
+                name="Ditto",
+            },
+            j_bootstraps={
+                name="Bootstraps",
+            },
+            j_brainstorm={
+                name="Smeargle",
+            },
+            j_bull={
+                name="Tauros",
+            },
+            j_burglar={
+                name="Thievul",
+            },
+            j_burnt={
+                name="Slugma",
+            },
+            j_business={
+                name="Business Card",
+            },
+            j_caino={
+                name="Arceus",
+            },
+            j_campfire={
+                name="Campfire",
+            },
+            j_card_sharp={
+                name="Meowscarada",
+            },
+            j_cartomancer={
+                name="Cartomancer",
+            },
+            j_castle={
+                name="Castle",
+            },
+            j_cavendish={
+                name="Cavendish Tropius",
+            },
+            j_ceremonial={
+                name="Ceremonial Dagger",
+            },
+            j_certificate={
+                name="Certificate",
+            },
+            j_chaos={
+                name="Mr. Mime the Clown",
+            },
+            j_chicot={
+                name="Yveltal",
+            },
+            j_clever={
+                name="Clever Umbreon",
+            },
+            j_cloud_9={
+                name="Cloud 9",
+            },
+            j_constellation={
+                name="Constellation",
+            },
+            j_crafty={
+                name="Crafty Absol",
+            },
+            j_crazy={
+                name="Crazy Leafeon",
+            },
+            j_credit_card={
+                name="Credit Card",
+            },
+            j_delayed_grat={
+                name="Delayed Gratification",
+            },
+            j_devious={
+                name="Devious Sylveon",
+            },
+            j_diet_cola={
+                name="Diet Cola",
+            },
+            j_dna={
+                name="DNA",
+            },
+            j_drivers_license={
+                name="Driver's License",
+            },
+            j_droll={
+                name="Droll Haxorus",
+            },
+            j_drunkard={
+                name="Drunkard",
+            },
+            j_duo={
+                name="The Duo",
+            },
+            j_dusk={
+                name="Dusk",
+            },
+            j_egg={
+                name="Exeggcute",
+            },
+            j_erosion={
+                name="Spinarak",
+            },
+            j_even_steven={
+                name="Even Serperior",
+            },
+            j_faceless={
+                name="Staryu",
+            },
+            j_family={
+                name="The Family",
+            },
+            j_fibonacci={
+                name="Fibonacci",
+            },
+            j_flash={
+                name="Flash Card",
+            },
+            j_flower_pot={
+                name="Flower Pot",
+            },
+            j_fortune_teller={
+                name="Fortune Teller",
+            },
+            j_four_fingers={
+                name="Four Fingers",
+            },
+            j_gift={
+                name="Gift Card",
+            },
+            j_glass={
+                name="Glass Joker",
+            },
+            j_gluttenous_joker={
+                name="Gluttenous Alcremie",
+            },
+            j_golden={
+                name="Gholdengo",
+            },
+            j_greedy_joker={
+                name="Greedy Greedent",
+            },
+            j_green_joker={
+                name="Maractus",
+            },
+            j_gros_michel={
+                name="Mankey",
+            },
+            j_hack={
+                name="The one Joke(r)",
+            },
+            j_half={
+                name="Dugtrio",
+            },
+            j_hallucination={
+                name="Porygon-Z",
+            },
+            j_hanging_chad={
+                name="Hanging Chad",
+            },
+            j_hiker={
+                name="Galarian Weasing",
+            },
+            j_hit_the_road={
+                name="Hit the Road",
+            },
+            j_hologram={
+                name="Hologram",
+            },
+            j_ice_cream={
+                name="Vanillite",
+            },
+            j_idol={
+                name="The Idol",
+            },
+            j_invisible={
+                name="Kecleon",
+            },
+            j_joker={
+                name="Eevee",
+            },
+            j_jolly={
+                name="Jolly Vaporeon",
+            },
+            j_juggler={
+                name="Juggler",
+            },
+            j_loyalty_card={
+                name="Loyalty Card",
+            },
+            j_luchador={
+                name="Luchador",
+            },
+            j_lucky_cat={
+                name="Meowth",
+            },
+            j_lusty_joker={
+                name="Lusty Salazzle",
+            },
+            j_mad={
+                name="Mad Flareon",
+            },
+            j_madness={
+                name="Scream Tail",
+            },
+            j_mail={
+                name="Mail-In Rebate",
+            },
+            j_marble={
+                name="Geodude",
+            },
+            j_matador={
+                name="Matador",
+            },
+            j_merry_andy={
+                name="Merry Cinccino",
+            },
+            j_midas_mask={
+                name="Yamask",
+            },
+            j_mime={
+                name="Mime Jr.",
+            },
+            j_misprint={
+                name="MissingNo.",
+            },
+            j_mr_bones={
+                name="Marowak",
+            },
+            j_mystic_summit={
+                name="Mystic Summit",
+            },
+            j_obelisk={
+                name="Stonjourner",
+            },
+            j_odd_todd={
+                name="Odd Zangoose",
+            },
+            j_onyx_agate={
+                name="Onyx Agate",
+            },
+            j_oops={
+                name="Oops! All Vivillons",
+            },
+            j_order={
+                name="The Order",
+            },
+            j_pareidolia={
+                name="Klefki",
+            },
+            j_perkeo={
+                name="Giratina",
+            },
+            j_photograph={
+                name="Zoroark Photograph",
+            },
+            j_popcorn={
+                name="Popcorn",
+            },
+            j_raised_fist={
+                name="Raised Fist",
+            },
+            j_ramen={
+                name="Ramen",
+            },
+            j_red_card={
+                name="Red Card",
+            },
+            j_reserved_parking={
+                name="Reserved Parking",
+            },
+            j_ride_the_bus={
+                name="Ride the Bus",
+            },
+            j_riff_raff={
+                name="Riff-Raff",
+            },
+            j_ring_master={
+                name="Showman",
+            },
+            j_rocket={
+                name="Cresselia",
+            },
+            j_rough_gem={
+                name="Rough Gem",
+            },
+            j_runner={
+                name="Lileep",
+            },
+            j_satellite={
+                name="Satellite Fight",
+            },
+            j_scary_face={
+                name="Scary Face",
+            },
+            j_scholar={
+                name="Scholar",
+            },
+            j_seance={
+                name="Séance",
+            },
+            j_seeing_double={
+                name="Seeing Double",
+            },
+            j_selzer={
+                name="Seltzer",
+            },
+            j_shoot_the_moon={
+                name="Shoot the Moon",
+            },
+            j_shortcut={
+                name="Shortcut",
+            },
+            j_sixth_sense={
+                name="Sixth Sense",
+            },
+            j_sly={
+                name="Sly Glaceon",
+            },
+            j_smeared={
+                name="Alolan Muk",
+            },
+            j_smiley={
+                name="Hisuian Voltorb",
+            },
+            j_sock_and_buskin={
+                name=" Girafarig and Farigiraf",
+            },
+            j_space={
+                name="Beheeyem",
+            },
+            j_splash={
+                name="Splash",
+            },
+            j_square={
+                name="Charjabug",
+            },
+            j_steel_joker={
+                name="Aggron",
+            },
+            j_stencil={
+                name="Who's that Pokémon?",
+            },
+            j_stone={
+                name="Onix",
+            },
+            j_stuntman={
+                name="Stunt Serperior",
+            },
+            j_supernova={
+                name="Supernova",
+            },
+            j_superposition={
+                name="Magnemite",
+            },
+            j_swashbuckler={
+                name="Dhelmise",
+            },
+            j_throwback={
+                name="Throwback",
+            },
+            j_ticket={
+                name="Golden Ticket",
+            },
+            j_to_the_moon={
+                name="To the Moon",
+            },
+            j_todo_list={
+                name="To Do List",
+            },
+            j_trading={
+                name="Trading Card",
+            },
+            j_tribe={
+                name="The Tribe",
+            },
+            j_triboulet={
+                name="Dialga",
+            },
+            j_trio={
+                name="The Trio",
+            },
+            j_troubadour={
+                name="Kricketune",
+            },
+            j_trousers={
+                name="Lost Trousers",
+            },
+            j_turtle_bean={
+                name="Trubbish",
+            },
+            j_vagabond={
+                name="Florette",
+            },
+            j_vampire={
+                name="Crobat",
+            },
+            j_walkie_talkie={
+                name="Walkie Talkie",
+            },
+            j_wee={
+                name="Vee Joker",
+            },
+            j_wily={
+                name="Wily Espeon",
+            },
+            j_wrathful_joker={
+                name="Wrathful Trevenant",
+            },
+            j_yorick={
+                name="Palkia"
+            },
+            j_zany={
+                name="Zany Jolteon",
+            }
+        }
+    }
+}
